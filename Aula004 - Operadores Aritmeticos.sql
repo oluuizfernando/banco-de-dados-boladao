c67953c4a14bfcbe2733c27f3573ac2d7744429c
oluuizfernando <104778712+oluuizfernando@users.noreply.github.com>
2023-10-27 21:48:31 -0300

    Operadores

---
+++ Aula004 - Operadores Aritmeticos.sql
@@ -0,0 +1,15 @@
+SELECT 1 + 1 AS MATH
+
+SELECT  1 + 3 AS SOMA,
+        5 - 10 AS [SUB(-1)],
+        1700 + (-900) AS NEG,
+        5 * (5 + 1) AS MULT_SOM,
+        -5 * 500 / 40 % 3 AS TODO
+
+SELECT
+    1 + '1' AS SOMA1, -- CONVERTE PARA INT
+    '1' + 1 AS SOMA2, -- CONVERTE PARA INT
+    '1' + '1' AS SOMA3 -- CONCATENAÇÃO
+
+SELECT
+    1 + '1A'
---
+++ Aula005 - Operadores Comparacao.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Person.Person
+WHERE FirstName = 'Gigi'
+
+SELECT * FROM Person.Person
+WHERE BusinessEntityID < 10
+
+SELECT * FROM Person.Person
+WHERE BusinessEntityID >= 10
+
+SELECT * FROM Person.Person
+WHERE BusinessEntityID <= 10
+
+SELECT * FROM Person.Person
+WHERE BusinessEntityID !< 10
